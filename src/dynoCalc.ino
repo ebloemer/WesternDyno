841234bcd93ccd03e4b754e95695bcae28dea087
Ethan Bloemert <ethanbloemert@gmail.com>
2026-03-07 14:03:14 -0500

    Final untested

--- src/dynoCalc.ino
+++ src/dynoCalc.ino
@@ -2,7 +2,15 @@
 #include <HX711.h>
 #include <ESP32Servo.h>
 #include <Adafruit_Neopixel.h>
+#include <HardwareSerial.h>
 
+// Setting up serial to UI
+HardwareSerial Interface(1); // Use UART1 (pins 17 and 18 for S3)
+#define UI_BAUD_RATE 115200
+#define UI_RX_PIN 17
+#define UI_TX_PIN 18
+
+// LED Properties
 #define LED_PIN 48
 #define LED_COUNT 1
 
@@ -10,37 +18,39 @@ Adafruit_NeoPixel led(LED_COUNT, LED_PIN, NEO_GRB + NEO_KHZ800);
 HX711 scale;
 Servo engineThrottle; // Using Servo library for throttle control (0-180 degrees)
 
-enum ledColor { OFF, RED, YELLOW, GREEN, BLUE }; // 0 = off, 1 = red, 2 = yellow, 3 = green, 4 = blue (no scale)
+enum ledColor { OFF, RED, YELLOW, GREEN, BLUE }; // LED color states
 ledColor currentColor = OFF;
 
 // Runtime
-const int emergencyPin = 14; // Pin to trigger emergency state (normally open switch to ground)
-const int resetPin = 13; // Pin to reset from emergency state (normally open switch to ground)
+#define emergencyPin 14 // Pin to trigger emergency state (normally open switch to ground)
+#define resetPin 13 // Pin to reset from emergency state (normally open switch to ground)
 bool emergency = false;
-unsigned long previousTime = 0;
+unsigned long ledPreviousMillis = 0;
+unsigned long UIPreviousMillis = 0;
+unsigned long PIDPreviousMillis = 0;
 
 // HX711
-const int DOUT = 4;  // Data pin
-const int CLK = 5;   // Clock pin
+#define DOUT 4  // Data pin
+#define CLK 5   // Clock pin
 bool scaleConnected = false;
 float scaleFactor = 2280.f; // Calibration factor for the load cell (adjust as needed)
 
 // Pump Pins
-const int pumpPulseSensor = 16;
-const int flowValvePin = 17;      // Flow solenoid PWM
-const int pressureValvePin = 18;  // Pressure solenoid PWM
+#define pumpPulseSensor 9 // Pump RPM sensor (magnetic pickup)
+#define flowValvePin 10      // Flow solenoid PWM
+#define pressureValvePin 11  // Pressure solenoid PWM
 
-const int flowValve = 1;      // PWM channel for flow valve
-const int pressureValve = 2;  // PWM channel for pressure valve
+#define flowValveChannel 1      // PWM channel for flow valve
+#define pressureValveChannel 2  // PWM channel for pressure valve
 
 // Engine Pins
-const int enginePulseSensor = 10;
-const int engineThrottlePin = 11;
+#define enginePulseSensor 6
+#define engineThrottlePin 7
 
 // Solenoid PWM values
-int flowValveValue = 0;      // Current flow valve setting (0-4095)
+float flowValveValue = 0;      // Current flow valve setting (0-4095)
 int flowValveFrequency = 250; // PWM frequency for flow valve
-int pressureValveValue = 0; // Current pressure valve setting (0-4095)
+float pressureValveValue = 0; // Current pressure valve setting (0-4095)
 int pressureValveFrequency = 250; // PWM frequency for pressure valve
 
 int pwmResolution = 12; // PWM resolution (12 bits for 0-4095 range)
@@ -59,34 +69,40 @@ int maxFlowValve = 0.98 * (pow(2, pwmResolution) - 1); // Maximum flow valve set
 int primarySprocket = 28; // Teeth on primary sprocket
 int secondarySprocket = 40; // Teeth on secondary sprocket
 
-float pressureP = 5; // Proportional gain for pressure control
-float pressureI = 1; // Integral gain for pressure control
-float pressureD = .5; // Derivative gain for pressure control
+float pressureP = 1.0; // Proportional gain for pressure control
+float pressureI = 0.2; // Integral gain for pressure control
+float pressureD = 0.05; // Derivative gain for pressure control
 float pressureIntegral = 0; // Integral term for pressure control
 float pressurePreviousError = 0; // Previous error for pressure control
+float pressurePreviousPreviousError = 0; // Previous previous error for delta PID
 unsigned long pressurePreviousTime = 0; // Previous time for pressure control PID
 
 float flowP = 0.1;     // Proportional gain for flow control
-float flowI = 0.02;     // Integral gain for flow control
-float flowD = 0.04;    // Derivative gain for flow control
+float flowI = 0.025;     // Integral gain for flow control
+float flowD = 0.01;    // Derivative gain for flow control
 float flowIntegral = 0; // Integral term for flow control
 float flowPreviousError = 0; // Previous error for flow control
+float flowPreviousPreviousError = 0; // Previous previous error for delta PID
 unsigned long flowPreviousTime = 0; // Previous time for flow control PID
 
 // Engine RPM variables:
-int engineThrottleValue = 0; // Current engine throttle setting (Min-Max degrees)
-int engineThrottleMin = 0; // Minimum engine throttle setting (0 degrees)
-int engineThrottleMax = 180; // Maximum engine throttle setting (180 degrees)
+int engineThrottleMin = 500;       // minimum throttle command %
+int engineThrottleMax = 2500;     // maximum throttle command %
+float engineThrottleValue = engineThrottleMin;     // PID output, 0-100%
+
+int servoMinAngle = 20;              // actual closed throttle position
+int servoMaxAngle = 110;             // actual full throttle position
 
 unsigned long enginePulse;
 unsigned long prevEnginePulse;
 int engineMagnets = 1;
 
-float engineP = 0.005;     // Proportional gain for engine RPM control
-float engineI = 0.001;     // Integral gain for engine RPM control
-float engineD = 0.0005;    // Derivative gain for engine RPM control
+float engineP = 0.001;     // Proportional gain for engine RPM control
+float engineI = 0.0002;     // Integral gain for engine RPM control
+float engineD = 0.00005;    // Derivative gain for engine RPM control
 float engineIntegral = 0; // Integral term for engine RPM control
 float enginePreviousError = 0; // Previous error for engine RPM control
+float enginePreviousPreviousError = 0; // Previous previous error for delta PID
 unsigned long enginePreviousTime = 0; // Previous time for engine RPM control PID
 
 // Pump RPM variables:
@@ -190,8 +206,8 @@ void pumpControl() {
   }
 
   // Write PWM values to valves
-  ledcWrite(flowValve, flowValveValue);
-  ledcWrite(pressureValve, pressureValveValue);
+  ledcWrite(flowValveChannel, flowValveValue);
+  ledcWrite(pressureValveChannel, pressureValveValue);
 }
 
 // PID control function for torque control
@@ -199,7 +215,20 @@ void torqueControl() {
   getTorque(); // Update torque reading
   float error = targetTorque - torque;
 
-  PIDControl(error, pressureIntegral, pressurePreviousError, pressureP, pressureI, pressureD, pressureValveValue, minPressureValve, maxPressureValve, pressurePreviousTime);
+  if(micros() > (pressurePreviousTime + 100000)) {
+    pressurePreviousTime = micros(); // Initialize previous time on first run
+    pressureIntegral = 0; // Reset integral term
+    pressurePreviousError = error; // Initialize previous error
+    pressurePreviousPreviousError = error; // Initialize previous previous error
+  }
+
+  /*absPIDControl(error, pressureIntegral, pressurePreviousError,
+                  pressureP, pressureI, pressureD, pressureValveValue,
+                  minPressureValve, maxPressureValve, pressurePreviousTime);*/
+  
+  deltaPIDControl(error, pressurePreviousError, pressurePreviousPreviousError,
+                  pressureP, pressureI, pressureD, pressureValveValue,
+                  minPressureValve, maxPressureValve, pressurePreviousTime);
 
   // RPM limiter (only near the top)
   getPumpRpm(); // Update pump RPM reading
@@ -220,7 +249,20 @@ void pumpRpmControl() {
   targetPumpRpm = constrain(targetPumpRpm, 0, maxPumpRpm); // Ensure target pump RPM does not exceed target engine RPM
   float error = targetPumpRpm - pumpRpm;
 
-  PIDControl(error, flowIntegral, flowPreviousError, flowP, flowI, flowD, flowValveValue, minFlowValve, maxFlowValve, flowPreviousTime);
+  if(micros() > (flowPreviousTime + 100000)){
+    flowPreviousTime = micros(); // Initialize previous time on first run
+    flowIntegral = 0; // Reset integral term
+    flowPreviousError = error; // Initialize previous error
+    flowPreviousPreviousError = error; // Initialize previous previous error
+  }
+
+  /*absPIDControl(error, flowIntegral, flowPreviousError,
+                  flowP, flowI, flowD, flowValveValue,
+                  minFlowValve, maxFlowValve, flowPreviousTime);*/
+  
+  deltaPIDControl(error, flowPreviousError, flowPreviousPreviousError,
+                  flowP, flowI, flowD, flowValveValue,
+                  minFlowValve, maxFlowValve, flowPreviousTime);
 }
 
 // PID control function for engine RPM control
@@ -228,10 +270,27 @@ void engineRpmControl() {
   getEngineRpm(); // Update engine RPM reading
   float error = targetEngineRpm - engineRpm;
 
-  PIDControl(error, engineIntegral, enginePreviousError, engineP, engineI, engineD, engineThrottleValue, engineThrottleMin, engineThrottleMax, enginePreviousTime);
+  if(micros() > (enginePreviousTime + 100000)) {
+    enginePreviousTime = micros(); // Initialize previous time on first run
+    engineIntegral = 0; // Reset integral term
+    enginePreviousError = error; // Initialize previous error
+    enginePreviousPreviousError = error; // Initialize previous previous error
+  }
+
+  /*absPIDControl(error, engineIntegral, enginePreviousError,
+                  engineP, engineI, engineD, engineThrottleValue,
+                  engineThrottleMin, engineThrottleMax, enginePreviousTime);*/
+  
+  deltaPIDControl(error, enginePreviousError, enginePreviousPreviousError,
+                  engineP, engineI, engineD, engineThrottleValue,
+                  engineThrottleMin, engineThrottleMax, enginePreviousTime);
+
+  engineThrottle.writeMicroseconds((int)engineThrottleValue); // Map throttle value to 0-100% for servo write
 }
 
-void PIDControl(float error, float &integral, float &previousError, float P, float I, float D, int &valveValue, int min, int max, unsigned long &previousTime) {
+void absPIDControl(float error, float &integral, float &previousError, 
+                    float P, float I, float D, float &valveValue,
+                    int min, int max, unsigned long &previousTime) {
   unsigned long now = micros();
   float dt = (now - previousTime) / 1000000.0;  // seconds
   if (dt <= 0) dt = 0.000001;                   // safety
@@ -251,26 +310,117 @@ void PIDControl(float error, float &integral, float &previousError, float P, flo
   previousError = error;
 }
 
+void deltaPIDControl(float error, float &previousError, float &previousPreviousError,
+                    float P, float I, float D, float &valveValue,
+                    int min, int max, unsigned long &previousTime) {
+  unsigned long now = micros();
+  float dt = (now - previousTime) / 1000000.0;   // seconds
+  if (dt <= 0) dt = 0.000001;                    // safety
+  previousTime = now;
+
+  // Incremental PID terms
+  float deltaP = P * (error - previousError);
+  float deltaI = I * error * dt;
+  float deltaD = D * ((error - 2 * previousError + previousPreviousError) / dt);
+
+  float deltaOutput = deltaP + deltaI + deltaD;
+
+  // Add change to existing output
+  valveValue += deltaOutput;
+
+  valveValue = constrain(valveValue, min, max);
+
+  // Shift stored errors
+  previousPreviousError = previousError;
+  previousError = error;
+}
+
 void printStatus() {
-  Serial.print("Engine RPM: ");
+  Serial.print("Mode: ");
+  if(mode == 0) Serial.print("MANUAL");
+  else if(mode == 1) Serial.print("TRQ CTRL");
+  else if(mode == 2) Serial.print("RPM CTRL");
+  else if(mode == 3) Serial.print("CVT CTRL");
+  else Serial.print("N/A");
+  Serial.print(" || Engine RPM: ");
   Serial.print(engineRpm);
   Serial.print(" - ");
   Serial.print(targetEngineRpm);
-  Serial.print(" || Pump RPM: ");
-  Serial.print(pumpRpm);
-  Serial.print(" - ");
-  Serial.print(targetPumpRpm);
-  Serial.print(" || Torque: ");
-  Serial.print(torque);
-  Serial.print(" - ");
-  Serial.print(targetTorque);
-  Serial.print(" || Flow Valve: ");
-  Serial.print(map(flowValveValue, 0, maxFlowValve, 0, 100));
-  Serial.print("% || Pressure Valve: ");
-  Serial.print(map(pressureValveValue, 0, maxPressureValve, 0, 100));
+  
+  if(mode == 1) {
+    Serial.print(" || Torque: ");
+    Serial.print(torque);
+    Serial.print(" - ");
+    Serial.print(targetTorque);
+    Serial.print("% || Pressure Valve: ");
+    Serial.print(map(pressureValveValue, minPressureValve, maxPressureValve, 0, 100));
+  }
+
+  if(mode == 2 || mode == 3) {
+    Serial.print(" || Pump RPM: ");
+    Serial.print(pumpRpm);
+    Serial.print(" - ");
+    Serial.print(targetPumpRpm);
+    Serial.print(" || Flow Valve: ");
+    Serial.print(map(flowValveValue, minFlowValve, maxFlowValve, 0, 100));
+  }
+
+
   Serial.print("% || Engine Throttle: ");
   Serial.print(map(engineThrottleValue, engineThrottleMin, engineThrottleMax, 0, 100));
   Serial.println("%");
+}
+
+void sendUIData() {
+  // Send data in a simple CSV format for easy parsing on the UI side
+  Interface.print(emergency ? "1" : "0"); // Emergency status
+  Interface.print(",");
+  Interface.print(engineRpm);
+  Interface.print(",");
+  Interface.print(targetEngineRpm);
+  Interface.print(",");
+  Interface.print(pumpRpm);
+  Interface.print(",");
+  Interface.print(targetPumpRpm);
+  Interface.print(",");
+  Interface.print(torque);
+  Interface.print(",");
+  Interface.print(targetTorque);
+  Interface.print(",");
+  Interface.print(map(flowValveValue, 0, maxFlowValve, 0, 100));
+  Interface.print(",");
+  Interface.print(map(pressureValveValue, 0, maxPressureValve, 0, 100));
+  Interface.print(",");
+  Interface.println(map(engineThrottleValue, engineThrottleMin, engineThrottleMax, 0, 100));
+}
+
+void recieveUICommands() {
+  // Check if data is available on the serial port
+  if (Interface.available() > 0) {
+    String command = Interface.readStringUntil('\n'); // Read until newline
+    // Parse command and update parameters accordingly
+    // Expected format: "EMERGENCY_STATUS,MODE,MANUAL_FLOW,MANUAL_PRESSURE,TARGET_TORQUE,TARGET_RPM,TARGET_ENGINE_RPM"
+    int index = 0;
+    String tokens[7];
+    while (command.length() > 0 && index < 7) {
+      int commaIndex = command.indexOf(',');
+      if (commaIndex == -1) {
+        tokens[index++] = command; // Last token
+        break;
+      } else {
+        tokens[index++] = command.substring(0, commaIndex);
+        command = command.substring(commaIndex + 1);
+      }
+    }
+    
+    if (index >= 1) emergency = tokens[0].toInt() == 1; // Set emergency flag based on first token
+    if (index >= 2) mode = tokens[1].toInt();
+    if (index >= 3) manualFlow = tokens[2].toInt();
+    if (index >= 4) manualPressure = tokens[3].toInt();
+    if (index >= 5) targetTorque = tokens[4].toFloat();
+    if (index >= 6) targetRpm = tokens[5].toFloat();
+    if (index >= 7) targetEngineRpm = tokens[6].toFloat();
+  }
 }
 
 void emergencyStop() {
@@ -283,8 +433,8 @@ void emergencyStop() {
     pressureValveValue = 0;
     engineThrottleValue = engineThrottleMin;
 
-    ledcWrite(flowValve, flowValveValue);
-    ledcWrite(pressureValve, pressureValveValue);
+    ledcWrite(flowValveChannel, flowValveValue);
+    ledcWrite(pressureValveChannel, pressureValveValue);
     engineThrottle.write(engineThrottleValue);
 
     flowIntegral = 0;
@@ -293,6 +443,9 @@ void emergencyStop() {
     flowPreviousError = 0;
     pressurePreviousError = 0;
     enginePreviousError = 0;
+    flowPreviousPreviousError = 0;
+    pressurePreviousPreviousError = 0;
+    enginePreviousPreviousError = 0;
     
     if(currentColor != RED) {
       colorSet(RED); // RED for emergency
@@ -314,9 +467,11 @@ void setup() {
   led.setBrightness(100);
   colorSet(RED); // RED to start
   
-  int halt = 1500; // Delay between setup steps for stability
+  int halt = 500; // Delay between setup steps for stability
 
   Serial.begin(115200);
+  Interface.begin(UI_BAUD_RATE, UI_RX_PIN, UI_TX_PIN); // RX, TX pins for UART1
+
   Serial.println("Dynamometer initializing...");
   delay(halt*2);
 
@@ -326,7 +481,7 @@ void setup() {
   Serial.println("Assigning pins...");
   int pins[] = {
     0,1,2,3,4,5,6,7,8,9,10,
-    11,12,13,14,15,16,17,18,19,20,21,
+    11,12,13,14,15,16,19,20,21,
     35,36,38,39,40,41,42,45,47,48
   };
 
@@ -339,8 +494,8 @@ void setup() {
   
   pinMode(emergencyPin, INPUT_PULLUP);
   pinMode(resetPin, INPUT_PULLDOWN);
-  pinMode(flowValve, OUTPUT);
-  pinMode(pressureValve, OUTPUT);
+  pinMode(flowValveChannel, OUTPUT);
+  pinMode(pressureValveChannel, OUTPUT);
   pinMode(enginePulseSensor, INPUT_PULLDOWN);
   pinMode(pumpPulseSensor, INPUT_PULLDOWN);
   pinMode(engineThrottlePin, OUTPUT);
@@ -351,10 +506,10 @@ void setup() {
 
   // Set PWM frequency and resolution
   Serial.println("Setting up PWM channels...");
-  ledcSetup(flowValve, flowValveFrequency, pwmResolution);
-  ledcSetup(pressureValve, pressureValveFrequency, pwmResolution);
-  ledcAttachPin(flowValvePin, flowValve);
-  ledcAttachPin(pressureValvePin, pressureValve);
+  ledcSetup(flowValveChannel, flowValveFrequency, pwmResolution);
+  ledcSetup(pressureValveChannel, pressureValveFrequency, pwmResolution);
+  ledcAttachPin(flowValvePin, flowValveChannel);
+  ledcAttachPin(pressureValvePin, pressureValveChannel);
 
   delay(halt);
 
@@ -386,13 +541,13 @@ void setup() {
     Serial.println("HX711 detected.");
     scaleConnected = true;
     
-    ledcWrite(flowValve, maxFlowValve); // Relieve any pressure in the system for accurate taring
+    ledcWrite(flowValveChannel, maxFlowValve); // Relieve any pressure in the system for accurate taring
     delay(2500);
 
     scale.set_scale(scaleFactor);
     scale.tare(50); // Tare with 50 samples for better accuracy
 
-    ledcWrite(flowValve, flowValveValue); // Close flow valve after taring
+    ledcWrite(flowValveChannel, flowValveValue); // Close flow valve after taring
   } else {
     Serial.println("HX711 NOT detected. Torque control unavailable.");
   }
@@ -408,12 +563,18 @@ void setup() {
 }
 
 void loop() {
-  pumpControl();
-  engineRpmControl();
-  
   //emergencyStop(); // Check for emergency stop condition every loop
+  pumpControl();
+  //engineRpmControl();
+  recieveUICommands();
+  sendUIData();
 
-  if (millis() - previousTime >= 500) {
+  if (millis() - UIPreviousMillis >= 16) { // Send data to UI every 100ms
+    sendUIData();
+    UIPreviousMillis = millis();
+  }
+
+  if (millis() - ledPreviousMillis >= 500) {
     if(!scaleConnected) {
       if(currentColor != YELLOW) {
         colorSet(YELLOW); // YELLOW if no scale connected
@@ -423,7 +584,7 @@ void loop() {
     } else {
       colorSet(GREEN); // GREEN for normal operation
     }
-    previousTime = millis();
+    ledPreviousMillis = millis();
     printStatus();
   }
 }
